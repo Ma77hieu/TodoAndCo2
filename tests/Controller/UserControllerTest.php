47dcb87d232143467fd3dedab15bd5baea42ff89
matthieu-ortlieb <matthieu.ortlieb@cafefrappe.fr>
2022-10-21 10:50:14 +0200

    Test user creation OK + implement automatic test database reset after tests

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -9,14 +9,14 @@ class UserControllerTest extends WebTestCase
     public function setUp(): void
     {
         $this->client = static::createClient();
+        $this->client->followRedirects();
     }
 
     /**
-     * Testing user creation failure when email already exists in databqse
+     * Testing user creation failure when email already exists in database
      */
     public function testforbiddenCreateUser(): void
     {
-
         $crawler = $this->client->request('GET', '/users/create');
         //finding the add_user button
         $buttonCrawlerNode = $crawler->selectButton('add_user');
@@ -37,6 +37,36 @@ class UserControllerTest extends WebTestCase
 
         $this->assertStringContainsString(
             'Cet email a déjà un compte associé',
+            $this->client->getResponse()->getContent()
+        );
+    }
+
+    /**
+     * Tests a correct user creation
+     */
+    public function testCreateUserOk(): void
+    {
+
+        $crawler = $this->client->request('GET', '/users/create');
+        //finding the add_user button
+        $buttonCrawlerNode = $crawler->selectButton('add_user');
+
+        // retrieve the Form object for the form belonging to this button
+        $form = $buttonCrawlerNode->form();
+
+        // set values on a form object
+        $form['user[username]'] = 'Fabien';
+        $form['user[password][first]'] = 'TestPwd2022!';
+        $form['user[password][second]']= 'TestPwd2022!';
+        $form['user[email]']= 'test2@hotmail.com';
+        $form['user[roles]']->select('ROLE_USER');
+
+
+        // submit the Form object
+        $this->client->submit($form);
+
+        $this->assertStringContainsString(
+            'utilisateur a bien été ajouté',
             $this->client->getResponse()->getContent()
         );
     }
